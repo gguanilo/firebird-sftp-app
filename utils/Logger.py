56afb6bf3ececadfb32cf1127dc44e88ecf147c4
gguanilo <gustavo.guanilo@neuralcg.com>
2025-01-06 07:46:20 -0500

    Configure dependencies and implemnt logger with color

--- utils/Logger.py
+++ utils/Logger.py
@@ -1,5 +1,5 @@
 import logging
-
+from colorlog import ColoredFormatter
 
 class Logger:
     """
@@ -13,11 +13,28 @@ class Logger:
 
         :param log_file: Name of the file where logs will be saved
         """
+        # File handler (for saving logs to a file)
+        file_handler = logging.FileHandler(log_file)
+        file_handler.setFormatter(logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s [%(filename)s]"
+        ))
+
+        # Console handler (for colored output in the terminal)
+        console_handler = logging.StreamHandler()
+        console_formatter = ColoredFormatter(
+            "%(log_color)s%(asctime)s - %(levelname)s - %(message)s [%(filename)s]",
+            log_colors={
+                "DEBUG": "white",
+                "INFO": "green",
+                "WARNING": "yellow",
+                "ERROR": "red",
+                "CRITICAL": "red,bg_white",
+            },
+        )
+        console_handler.setFormatter(console_formatter)
+
+        # Configure the root logger
         logging.basicConfig(
             level=logging.INFO,
-            format="%(asctime)s - %(levelname)s - %(message)s [%(filename)s]",
-            handlers=[
-                logging.FileHandler(log_file),
-                logging.StreamHandler()
-            ]
+            handlers=[file_handler, console_handler]
         )
